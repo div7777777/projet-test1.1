6df920d9781c20e3690ef32c855808f84a96ea13
div7777777 <99870561+div7777777@users.noreply.github.com>
2022-10-05 17:14:23 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+// barre de recherche
+function search_icones(){
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('maps');
+    for (i=0; i<x.length; i++){
+        if (!x[i].innerHTML.toLocaleLowerCase().includes(input)){
+            x[i].style.display="none";
+        }
+        else{
+            x[i].style.display="list-item";
+  
+        }
+    }
+  }
+//   meni pied de page
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
